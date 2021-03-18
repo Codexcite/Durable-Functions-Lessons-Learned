405ae93fe5b8b6a3dd053fd972d1e24b541b75a4
Vlad Horbovanu <vlad@codexcite.com>
2021-03-18 11:57:55 -0600

    added ThrottleOrchestrator

---
+++ Codexcite.DurableFunctions/Examples/4 EntityOrchestration/ThrottleOrchestrator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Codexcite.DurableFunctions.Examples.EntityOrchestration;
+using Codexcite.DurableFunctions.Extensions;
+using Codexcite.DurableFunctions.Utils;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Serilog.Events;
+
+namespace Codexcite.DurableFunctions.Examples._4_EntityOrchestration
+{
+	public class ThrottleOrchestrator : BaseLogged
+	{
+		[FunctionName(nameof(ThrottleResult))]
+		public async Task<object?> ThrottleResult(
+			[OrchestrationTrigger] IDurableOrchestrationContext context)
+		{
+			var input = context.GetInput<ThrottledFunctionCall>();
+			using var propsDisposable = context.PushLoggerProperties();
+			_log.Enter(LogEventLevel.Information, arguments: new object?[] { input });
+
+			var throttleProxy = context.CreateEntityProxy<IThrottleEntity>(input.ThrottleKey);
+			var scheduleTime = await throttleProxy.GetNextScheduleTime();
+
+			await context.CreateTimer(scheduleTime.UtcDateTime, CancellationToken.None);
+			var result = await context.CallActivityAsync<object?>(input.Name, input.Input);
+
+			_log.Exit(LogEventLevel.Information, arguments: new object?[] { input }, returnValue: result);
+			return result;
+		}
+
+		[FunctionName(nameof(ThrottleVoid))]
+		public async Task ThrottleVoid(
+			[OrchestrationTrigger] IDurableOrchestrationContext context)
+		{
+			var input = context.GetInput<ThrottledFunctionCall>();
+
+			using var propsDisposable = context.PushLoggerProperties();
+			_log.Enter(LogEventLevel.Information, arguments: new object?[] { input });
+
+			var throttleProxy = context.CreateEntityProxy<IThrottleEntity>(input.ThrottleKey);
+			var scheduleTime = await throttleProxy.GetNextScheduleTime();
+
+			await context.CreateTimer(scheduleTime.UtcDateTime, CancellationToken.None);
+			await context.CallActivityAsync(input.Name, input.Input);
+			_log.Exit(LogEventLevel.Information, arguments: new object?[] { input });
+		}
+	}
+}
---
+++ Codexcite.DurableFunctions/Examples/4 EntityOrchestration/ThrottledFunctionCall.cs
@@ -0,0 +1,16 @@
+﻿namespace Codexcite.DurableFunctions.Examples._4_EntityOrchestration
+{
+	public class ThrottledFunctionCall
+	{
+		public ThrottledFunctionCall(string name, string? throttleKey = null, object? input = null)
+		{
+			Name = name;
+			ThrottleKey = throttleKey ?? name;
+			Input = input;
+		}
+
+		public string Name { get; set; }
+		public object? Input { get; set; }
+		public string ThrottleKey { get; set; }
+	}
+}
